450fb473391b0eb96a102976eb12e6ea0b42e9fe
DeshonS <deshonjewell@gmail.com>
2024-09-03 16:21:57 -0500

    added condition for negative numbers

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{:d}".format(number % 10, end=""))
+    if number > 0:
+        print("{:d}".format(number % 10), end="")
+    else:
+        print("{:d}".format(number % -10), end="")
