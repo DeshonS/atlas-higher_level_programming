4e0b649b00e7c26fc2ed35703d77c9993acf5708
DeshonS <deshonjewell@gmail.com>
2024-09-03 16:12:25 -0500

    added a for loop to identify only the charachter being referenced from the string for each loop

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) in range(97, 129):
-        i = chr(ord(i) + (ord("A") - ord("a")))
-    print("{:s}".format(i), end="")
-print("")
+    for i in str:
+        if ord(i) in range(97, 129):
+            i = chr(ord(i) + (ord("A") - ord("a")))
+        print("{:s}".format(i), end="")
+    print("")
