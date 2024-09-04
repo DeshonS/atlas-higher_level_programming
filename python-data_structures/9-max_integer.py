87b643949e20edd531733e39471e1ae971fbdbcc
DeshonS <deshonjewell@gmail.com>
2024-09-03 22:02:10 -0500

    completed 9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    value = my_list[0]
+    for i in my_list[1:]:
+        if i > value:
+            value = i
+    return (value)
