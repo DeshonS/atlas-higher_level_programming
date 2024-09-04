601e34f7c6d3df0e17adfc134d22f7245bdf4b6d
DeshonS <deshonjewell@gmail.com>
2024-09-03 23:20:18 -0500

    done

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     value = 0
-    for i in a_dictionary:
-        if i > value:
-            value = i
-    return value
+    if a_dictionary:
+        return max(a_dictionary)
