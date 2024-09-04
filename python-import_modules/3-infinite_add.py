a2007753d16877a988c2ec883ad0767f91e1e5ec
DeshonS <deshonjewell@gmail.com>
2024-09-03 19:06:59 -0500

    added the loop directly to the format for print to attempt to fix an iteration error

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    for i in argv[1:]:
-        print("{}".format(sum(int(i))))
+    print("{}".format(sum(int(i) for i in argv[1:])))
