0bd836a468639e5ce4759f618b053dde01c65bd0
DeshonS <deshonjewell@gmail.com>
2024-09-06 21:39:16 -0500

    completed 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+square class
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """
+        defining size and conditions
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        defining area function
+        """
+        return(self.__size**2)
