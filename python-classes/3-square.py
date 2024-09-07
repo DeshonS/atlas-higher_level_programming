910a6239b7599493befaabd97df9e45b1de9cb25
DeshonS <deshonjewell@gmail.com>
2024-09-06 21:54:52 -0500

    documentation

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 class Square:
-    """Represents a square."""
+    """square class"""
 
     def __init__(self, size=0):
-        """Initialize a new Square instance.
-
-        Args:
-            size (int): The size of the square's side, which must be a non-negative integer.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
+        """initialization method"""
+        if type(size) != int:
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
 
     def area(self):
-        """Return the current square area."""
-        return self.__size ** 2
+        """square value"""
+        return (self.__size**2)
