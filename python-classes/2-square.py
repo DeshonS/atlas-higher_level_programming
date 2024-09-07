d42f0d471658ede8327b3da929b8130b1c69e71c
DeshonS <deshonjewell@gmail.com>
2024-09-06 21:30:41 -0500

    changed type to size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     defining size and errors
     """
     def __init__(self, size=0):
-        if size(type) != int:
+        if size(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
