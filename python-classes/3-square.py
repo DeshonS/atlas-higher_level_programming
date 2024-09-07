4c9b0bd1b7fe14d026b48300063ca4412d85b7e6
DeshonS <deshonjewell@gmail.com>
2024-09-06 21:40:53 -0500

    moved documentation around

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-square class
-"""
 
 
 class Square:
+    """
+    square class
+    """
     def __init__(self, size=0):
         """
         defining size and conditions
