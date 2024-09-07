e6b6441e766805ebf5b4ad55fd6e2abe6c0d0e69
DeshonS <deshonjewell@gmail.com>
2024-09-06 21:34:48 -0500

    changed != to "is not" because thats what pycodestyle is telling me to do

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     defining size and errors
     """
     def __init__(self, size=0):
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
