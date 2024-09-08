1dba62323a8ce27f44132e9cdaf6a1ea9d67c4f0
DeshonS <deshonjewell@gmail.com>
2024-09-08 10:49:17 -0500

    completed 5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Making a square using all of our tools"""
+
+
+class Square:
+    """create class square"""
+    def __init__(self, size=0):
+        """initialize by assigning size a value"""
+        self.size = size
+    
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @self.setter
+    def size(self, value):
+        """setter to assign size a value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """return the area of a square with size valued size"""
+        return (self.__size**2)
+
+    def my_print(self):
+        """print square"""
+        if self.__size = 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("{}".format("#"), end="")
+            print()
