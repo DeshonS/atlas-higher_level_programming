a2b0cab87ff71b259268321d0b10b20e7b31f4fa
DeshonS <deshonjewell@gmail.com>
2024-09-09 15:18:15 -0500

    changed "is not" to "!="

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,11 +2,11 @@
 """add_integer - function that adds two given numbers together and returns the result"""
 
 
-def add_integer(a=0, b=98):
+def add_integer(a, b=98):
     """defining function name add_integer"""
-    if type(a) is not int or float:
+    if type(a) != int or float:
         raise TypeError("a must be an integer")
-    if type(b) is not int or float:
+    if type(b) != int or float:
         raise TypeError("b must be an integer")
     else:
         return (a + b)
