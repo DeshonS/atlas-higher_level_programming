e6475bf1e62e6d986de04c0231edc1e1a1d8f39b
DeshonS <deshonjewell@gmail.com>
2024-09-09 15:16:17 -0500

    initialized variable a

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 """add_integer - function that adds two given numbers together and returns the result"""
 
 
-def add_integer(a, b=98):
+def add_integer(a=0, b=98):
     """defining function name add_integer"""
     if type(a) is not int or float:
         raise TypeError("a must be an integer")
