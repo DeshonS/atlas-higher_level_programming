2e4c6521f0b8a23810fc42349fdd3f4f1ba8211a
DeshonS <deshonjewell@gmail.com>
2024-09-09 15:15:05 -0500

    added documentation for module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""add_integer - function that adds two given numbers together and returns the result"""
+
+
 def add_integer(a, b=98):
+    """defining function name add_integer"""
     if type(a) is not int or float:
         raise TypeError("a must be an integer")
     if type(b) is not int or float:
