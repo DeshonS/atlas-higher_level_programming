4a02896de12c990a1f1394ac5fcc0b501d6f3d39
DeshonS <deshonjewell@gmail.com>
2024-09-09 10:22:22 -0500

    changed is not to != for len(value in position setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -36,7 +36,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) is not 2:
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
