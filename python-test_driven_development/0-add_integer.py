83a70ccb45f3bd37f93023bfa0b6f49fd4025bfb
DeshonS <deshonjewell@gmail.com>
2024-09-09 16:10:55 -0500

    shortened a comment line

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""add_integer - function that adds two given numbers together and returns the result"""
+"""add_integer - function that adds two given numbers together"""
 
 
 def add_integer(a, b=98):
