bd27a4b31831d3c043300246d71e8fb9b3eb0d1a
DeshonS <deshonjewell@gmail.com>
2024-09-10 16:37:43 -0500

    indentation error that didnt get caught by the checker fixed on repr

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -55,6 +55,6 @@ class Rectangle:
         else:
             return "\n".join(["#"*self.__width for _ in range(self.__height)])
 
-        def __repr__(self):
-            """repr"""
-            return ("Rectangle({}, {})".format(self.__width, self.__height))
+    def __repr__(self):
+        """repr"""
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
