b958037c721da43b2630356be1d68858272872a5
DeshonS <deshonjewell@gmail.com>
2024-09-10 16:31:57 -0500

    done

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -56,5 +56,5 @@ class Rectangle:
             return "\n".join(["#"*self.__width for _ in range(self.__height)])
 
         def __repr__(self):
-        """repr"""
-        return ("Rectangle({}, {})".format(self.__width, self.__height))
+            """repr"""
+            return ("Rectangle({}, {})".format(self.__width, self.__height))
