db27a4b573dfeb2d034ee25b4bc705f0e921555d
DeshonS <deshonjewell@gmail.com>
2024-09-10 22:49:00 -0500

    done

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""raise_exception_msg - raises name exception with message"""
+
+
+def raise_exception_msg(message=""):
+    """init raise_exception_msg"""
+    raise NameError(message)
