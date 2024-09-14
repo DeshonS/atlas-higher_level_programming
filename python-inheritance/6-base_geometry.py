abb564963652bcaf0f44eac50bc28a7d279310df
DeshonS <deshonjewell@gmail.com>
2024-09-14 11:48:55 -0500

    done

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    class BaseGeometry
+    
+    def area(self):
+    raises exception error
+"""
+
+
+class BaseGeometry:
+    """init BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
