112b0e0e2662b80e4f6fd38027537e683161a178
DeshonS <deshonjewell@gmail.com>
2024-09-17 13:57:09 -0500

    done

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""reads a file"""
+
+
 def read_file(filename=""):
+    """init read_file"""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read())
