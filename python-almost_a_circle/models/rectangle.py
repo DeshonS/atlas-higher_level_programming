b36d07b910b9e724dffa255c4c6378581f0dd46f
DeshonS <deshonjewell@gmail.com>
2024-09-20 09:09:02 -0500

    added getters and setters for all variables and added documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,3 +12,43 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+        
+    @property
+    def height(self):
+        """returns the height"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """sets the height"""
+        self.__height = value
+        
+    @property
+    def width(self):
+        """returns the width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """sets the width"""
+        self.__width = value
+        
+    @property
+    def x(self):
+        """returns the value x"""
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """sets the value x"""
+        self.__x = value
+        
+    @property
+    def y(self):
+        """returns the value y"""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """sets the value y"""
+        self.__y = value
