3808fd8c1649a7b254da84570a234e71c4821c68
DeshonS <deshonjewell@gmail.com>
2024-09-20 08:43:00 -0500

    added duocumentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""class Base"""
+
+
 class Base:
+    """init class"""
     __nb_objects = 0
     def __init__(self, id=None):
+        """initialization"""
         if id is not None:
             self.id = id
         else:
