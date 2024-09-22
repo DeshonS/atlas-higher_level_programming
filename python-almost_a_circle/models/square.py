08d23b1fe0f7be426d4ffd323ddd41bc45493b0a
DeshonS <deshonjewell@gmail.com>
2024-09-22 09:26:39 -0500

    lots of formatting changes removing whitespace

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,22 +2,23 @@
 """Square class"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """class declaration"""
     def __init__(self, size, x=0, y=0, id=None):
         """initialization"""
         super().__init__(size, size, x, y, id)
-        
+
     def __str__(self):
         """return info about Square"""
         return "[Square] ({}) {}/{} - {}".format(self.id, 
-        self.x, self.y, self.width)
-    
+            self.x, self.y, self.width)
+
     @property
     def size(self):
         """gets the size"""
         return self.width
-    
+
     @size.setter
     def size(self, value):
         """sets the size"""
