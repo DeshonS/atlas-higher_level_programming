c68e3d9ef4ac7a39f47aa03067967d463f46663f
DeshonS <deshonjewell@gmail.com>
2024-09-22 09:28:14 -0500

    splot line 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,7 +11,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """return info about Square"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, 
+                                                 self.x, self.y, self.width)
 
     @property
     def size(self):
