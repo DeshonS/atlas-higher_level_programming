1c1c1b32a0ff971f99019c8b49a53c99913b31f0
DeshonS <deshonjewell@gmail.com>
2024-09-23 08:49:35 -0500

    added to_dictionary to Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,8 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns the dictionary representation of rectangle"""
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
