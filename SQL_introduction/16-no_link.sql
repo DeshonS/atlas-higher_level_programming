92720ad0629aaf4aa768ecf95758081fe11224bb
DeshonS <deshonjewell@gmail.com>
2024-10-10 09:14:54 -0500

    added entire command on one line

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,2 @@
 -- lists all records from second_table except rows without a name
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
