c89fd6edea035dd3775bc1d50a969a9a656dee64
DeshonS <deshonjewell@gmail.com>
2024-10-11 12:18:31 -0500

    completed 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- creates a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL GENERATED PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    UNIQUE (id)
+    UNIQUE (id),
+    PRIMARY KEY (id)
 )
