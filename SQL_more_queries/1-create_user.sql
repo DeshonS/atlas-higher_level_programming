188eeebb6fb4140fc2dc2d7288499ef7d67ad310
DeshonS <deshonjewell@gmail.com>
2024-10-11 08:30:54 -0500

    added NOT to first line

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a new user
-CREATE USER 'user_0d_1'@'localhost' IF EXISTS IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
