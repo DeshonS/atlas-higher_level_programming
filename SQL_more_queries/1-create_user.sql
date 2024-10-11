336181327ccfbee7c0d9197ed34565fe7defd30f
DeshonS <deshonjewell@gmail.com>
2024-10-11 08:27:39 -0500

    testing the option

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a new user
 CREATE USER 'user_0d_1'@'localhost' IF EXISTS IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
