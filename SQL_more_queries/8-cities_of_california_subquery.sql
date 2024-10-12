81ec9aa413ca1eb995109038ff9cb48aed29769c
DeshonS <deshonjewell@gmail.com>
2024-10-12 10:20:36 -0500

    removed line 3

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- lists cities in California from database
 SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name = 'California';
-ORDER BY cities.id ASC;
