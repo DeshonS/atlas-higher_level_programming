93a552841ceab7ab7abb7345a3a2757e74cdb6aa
DeshonS <deshonjewell@gmail.com>
2024-10-14 10:58:59 -0500

    added another genre

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- lists all shows in hbtn_0d_tvshows that have a genre linked
 SELECT DISTINCT tv_shows.title, tv_genres.name FROM tv_shows
 JOIN tv_genres ON tv_shows.id = tv_genres.id
-ORDER BY tv_shows.title AND tv_show_genres.id;
+ORDER BY tv_shows.title AND tv_show_genres.genre_id;
