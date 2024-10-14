bb8ca6735ec507b6dde7fe8e338ea0d0227413d1
DeshonS <deshonjewell@gmail.com>
2024-10-14 11:31:59 -0500

    fixed ORDER BY error

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- lists all genres from database and displays the number of shows linked to each
 SELECT tv_genres.name AS genres, COUNT(tv_show_genres.show_id) AS number_of_shows
 JOIN genres ON tv_show_genres.id = genres_id
-ORDER BY number_of_shows DESC, tv_genres.name ASC;
+ORDER BY number_of_shows DESC, genres ASC;
