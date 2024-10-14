e335732efd2cb8fcf0fb7bc8819eca22e206256c
DeshonS <deshonjewell@gmail.com>
2024-10-14 11:03:00 -0500

    changed tv_show_id to show_id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- lists all shows in hbtn_0d_tvshows that have a genre linked
 SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title AND tv_show_genres.genre_id ASC;
