e449d2c0475f11e153fd59dcf895fb1e0dea7f97
DeshonS <deshonjewell@gmail.com>
2024-10-15 18:16:18 -0500

    added permissions

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
     db = connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cur.fetchall()
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    states = curs.fetchall()
     for state in states:
         print(state)
     cur.close()
