1e063c74c5de63659cad4f2abf702d9ca56bbedf
DeshonS <deshonjewell@gmail.com>
2024-10-15 08:43:22 -0500

    got my tables mixed up

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- lists all comedy shows in database
 SELECT tv_shows.title AS title FROM tv_shows
-JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
