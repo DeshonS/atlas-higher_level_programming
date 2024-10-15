96657fcb727d5da1398be26494154eef9d5fdadc
DeshonS <deshonjewell@gmail.com>
2024-10-15 08:42:15 -0500

    added title and fixed variable name

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- lists all comedy shows in database
-SELECT tv_shows.title FROM tv_shows
+SELECT tv_shows.title AS title FROM tv_shows
 JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
-WHERE genres.name = 'Comedy'
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
