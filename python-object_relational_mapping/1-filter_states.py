3a5bd5656b8daa3afea7e1f625330305b98c8c7a
DeshonS <deshonjewell@gmail.com>
2024-10-15 22:06:32 -0500

    added case for lowercase

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' OR 'n%' ORDER BY id ASC")
     states = curs.fetchall()
     for state in states:
         print(state)
