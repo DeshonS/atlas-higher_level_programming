4e580cdd0ad5d637eea8ed596a8b768960ac68b8
DeshonS <deshonjewell@gmail.com>
2024-10-16 16:21:37 -0500

    completed 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""displays all values in state that meet arg"""
+
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(search))
+        result = cur.fetchall()
+        for res in result:
+            print(res)
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("Error {}".format(e))
+        sys.exit(1)
+    
