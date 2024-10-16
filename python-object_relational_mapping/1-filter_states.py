96ca182d8c2907dc8c69d77315edb27e737995f4
DeshonS <deshonjewell@gmail.com>
2024-10-15 22:00:55 -0500

    added arguments from task 0 to try to fix error

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,11 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
     curs = db.cursor()
     curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = curs.fetchall()
