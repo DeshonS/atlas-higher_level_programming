dcc9634f51d97b9766da4cf83b86e1da46b095c3
DeshonS <deshonjewell@gmail.com>
2024-10-16 11:19:14 -0500

    added states tag to name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name = {}".format(sys.argv[4]))
+    cur.execute("SELECT id, name FROM states WHERE states.name = {}".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
