5d3acfe0b1809cf127149bd458c2a1f956d9a91f
DeshonS <deshonjewell@gmail.com>
2024-10-16 16:31:19 -0500

    completed 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists all cities from database"""
+
+
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id")
+    result = cur.fetchall()
+    for res in result:
+        print(res)
+    cur.close()
+    db.close()
