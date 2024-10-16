c481ae92d9abc0d7370720a39dcbd5f719981636
DeshonS <deshonjewell@gmail.com>
2024-10-16 11:05:02 -0500

    changed search variable to its base

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,11 +9,10 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    search = sys.argv[4]
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id".format(search))
+    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
