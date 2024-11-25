a6b89d9f750be1694714c078de16ce862594ad36
DeshonS <deshonjewell@gmail.omc>
2024-11-25 20:43:42 +0000

    done

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,16 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const wedgeId = '18';
 request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.log(error)
+if (error) {
+  console.log(error)
+}
+  try {
+    const films = JSON.parse(body).results;
+    const wedgeMovies = films.filter(film =>
+      film.characters.some(characterUrl => characterUrl.includes(`/people/${wedgeId}/`))
+    );
+    console.log(wedgeMovies.length);
+  } catch (parseError) {
+    console.log(parseError);
   }
-    try {
-      const films = JSON.parse(body).results;
-      const wedgeMovies = films.filter(film =>
-        film.characters.some(characterUrl => characterUrl.includes(`/people/${wedgeId}/`))
-      );
-      console.log(wedgeMovies.length);
-    } catch (parseError) {
-      console.log(parseError);
-    }
-  });
+});
