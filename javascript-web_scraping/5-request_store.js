0afa9489852d7fb6f1a07cc11cc58f3fb89ce0c6
DeshonS <deshonjewell@gmail.com>
2024-11-26 16:39:41 +0000

    completed

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request.get(url, (response, body) => {
-  fs.writeFileSync(filePath, body, 'utf-8');
+request.get(url, (error, response, body) => {
+if (error) {
+  console.log(error);
+}
+fs.writeFileSync(filePath, body, 'utf-8');
 });
